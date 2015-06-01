554d10b4931ab8838af72d09b005c200f7ada6f2
nicbiddell <nicbiddell@gmail.com>
2015-06-01 16:17:54 +0900

    Basic 16x16 with random color

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
-    //$('.square').css({"height": "4px", "width": "4px"});
-    $("#grid").append('<div class="square"></div>');
+     //Set up the initial 16x16 grid.
+    for(var i = 0; i < 256; i++)
+    {
+        $("#grid").append('<div class="square"></div>');
+    }
+    
+    $(".square").mouseenter(function() {
+         var $color = "#" + (Math.random()*0xFFF000<<0).toString(16);
+         var $hue = Math.floor(Math.random() * 360);
+         var $op = Math.floor(Math.random() * 100);
+
+        /*$(this).css("background-color", $color);*/
+        $(this).css({'background-color': $color, 'hue-rotate': $hue, 'opacity': $op});
+     });
+
 });
+
+/*
+$(document).ready(function() {
+    
+   
+     
+    $(".square").mouseeenter(function() {
+       $(this).css("background-color": "black"); 
+    });
+});*/
+
+
