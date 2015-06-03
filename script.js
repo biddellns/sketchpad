931c28b173f797831d560d475404c81ba9cb370b
nicbiddell <nicbiddell@gmail.com>
2015-06-04 00:45:45 +0900

    Final base version

--- script.js
+++ script.js
@@ -5,25 +5,64 @@ $(document).ready(function() {
         $("#grid").append('<div class="square"></div>');
     }
     
+    //If the mouse enters a square, assign a random color with a random hue and opacity. 
     $(".square").mouseenter(function() {
          var $color = "#" + (Math.random()*0xFFF000<<0).toString(16);
          var $hue = Math.floor(Math.random() * 360);
          var $op = Math.floor(Math.random() * 100);
 
-        /*$(this).css("background-color", $color);*/
         $(this).css({'background-color': $color, 'hue-rotate': $hue, 'opacity': $op});
      });
+     
+     //If a user clicks this button, all squares are cleared.
+     $('#clear').click(function(){
+         var $clear = confirm("Are you sure you want to clear? ");
+                  
+         if ($clear) {
+         $(".square").css("background-color", "transparent");
+         }
+     });
+
+     //Change number of squares based on user input.
+     $('#change-size').click(function() {
+         var $numRows = prompt("How many rows do you want to use? ");
+         var $numCol = prompt("How many columns do you want to use? ");
+         
+         //Verify that the user entered numbers.
+         if ($numRows == "" || $numCol == "" ||            //No empty input
+	     isNaN($numRows) || isNaN($numCol))            //Inputs are numbers
+         {      
+             alert("Oops! You can only input numbers! ");
+         }
+
+         var $squareHeight = Math.round(720 / $numRows);
+         var $squareWidth = Math.round(720 / $numCol);
+
+         $('.square').css({'height': $squareHeight, 'width': $squareWidth});
+
+         //Clear current sketch
+         $(".square").css("background-color", "transparent");
+         
+         //Create a new set of squares
+         for (var i = 0; i < $numRows; i++)
+         {
+             if (i != 0) 
+             {
+                 $('#grid').append('<br><div class="square"></div>');
+             }
+             else 
+             {
+                   $('#grid').append('<div class="square"></div>');
+             }
+             
+             for (var j = 0; j < $numCol; j++)
+             {
+                   $('#grid').append('<div class="square"></div>');
+             }
+         }
+
+     });  
 
 });
-
-/*
-$(document).ready(function() {
-    
-   
-     
-    $(".square").mouseeenter(function() {
-       $(this).css("background-color": "black"); 
-    });
-});*/
 
 
